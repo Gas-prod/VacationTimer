9fb973135f0ad407da3639587d09aaa38934a02c
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2020-12-28 13:43:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts("https://gas-prod.github.io/VacationTimer/sw-toolbox.js");
-toolbox.precache(["/index.html", "/style.css"]);
+toolbox.precache(["/index.html", "/site.css", "/img/arrow_icon.svg", "/img/location_icon.svg", "/img/location_icon2.svg", "/icon/192x192.png", "/icon/512x512.png"]);
 
-toolbox.router.get("/*", toolbox.networkFirst, {
+/*toolbox.router.get("/*", toolbox.networkFirst, {
     networkTimeoutSeconds: 5
-})
+})*/
