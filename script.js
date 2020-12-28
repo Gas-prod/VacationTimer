bd02c8beacbff676fce32989b25bd99ced8f73f7
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2020-12-28 19:27:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -500,16 +500,14 @@ window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     deferredPrompt = e;
   
-    window.addEventListener('load', (e) => {
-        deferredPrompt.prompt();
+    deferredPrompt.prompt();
 
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        })
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
     })
 })
