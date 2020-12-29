ed303e279399ef1a7762064ffe2a2bb92511531b
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2020-12-29 16:04:57 +0100

    Add files via upload

--- A2HS.js
+++ A2HS.js
@@ -1,22 +1,34 @@
 var deferredPrompt;
 
+const installShadow = document.querySelector(".install-shadow");
+const installButton = document.querySelector(".install-button");
+const dismissButton = document.querySelector(".dismiss-button");
+
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
 
-    //window.addEventListener('load', (e) => {
+    installShadow.style.display = "block";
+
+    installButton.addEventListener('click', (e) => {
+        installShadow.style.display = "none";
         // Show the prompt
         deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
         deferredPrompt.userChoice.then((choiceResult) => {
+
             if (choiceResult.outcome === 'accepted') {
                 console.log('User accepted the A2HS prompt');
             } else {
                 console.log('User dismissed the A2HS prompt');
             }
             deferredPrompt = null;
-        });
-    //});
-});
+        })
+    })
+})
+
+dismissButton.addEventListener("click", function(){
+    installShadow.style.display = "block";
+})
