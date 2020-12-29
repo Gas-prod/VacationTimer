026d851147cf7c7868ade4c4c03ac3df26c97dba
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2020-12-29 14:49:21 +0100

    Add files via upload

--- A2HS.js
+++ A2HS.js
@@ -1,18 +1,17 @@
-
 window.addEventListener('beforeinstallprompt', (e) => {
     var triggerEvent = e;
     this.showPromptToInstall = true;
 
-    installprompt.addEventListener('click', (e) => {
-        triggerEvent.prompt();
-        //user response
-        triggerEvent.userChoice.then((response) => {
-            if (response.outcome === 'accepted') {
-                console.log('A2HS accepted');
-            } 
-            else{
-                console.log('A2HS refused');
-            }
-        })
+    triggerEvent.prompt();
+
+    //user response
+    triggerEvent.userChoice.then((response) => {
+        if (response.outcome === 'accepted') {
+            console.log('A2HS accepted');
+        } 
+        else{
+            console.log('A2HS refused');
+        }
     })
 })
+triggerEvent = null;
