c02e177cabf8584b892679f6544d77726c5767d9
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2020-12-29 17:45:20 +0100

    Add files via upload

--- A2HS.js
+++ A2HS.js
@@ -5,39 +5,35 @@ const installShadow = document.querySelector(".install-shadow");
 const installButton = document.querySelector(".install-button");
 const dismissButton = document.querySelector(".dismiss-button");
 
-if (installAlertOpen == true) {
-    window.addEventListener('beforeinstallprompt', (e) => {
+window.addEventListener('beforeinstallprompt', (e) => {
+    if(installAlertOpen == true){
         // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
 
-        installShadow.style.display = "block";
+    installShadow.style.display = "block";
 
-        installButton.addEventListener('click', (e) => {
-            // Show the prompt
-            deferredPrompt.prompt();
-            // Wait for the user to respond to the prompt
-            deferredPrompt.userChoice.then((choiceResult) => {
+    installButton.addEventListener('click', (e) => {
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
 
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt');
-                    installAlertOpen = false;
-                    installShadow.style.display = "none";
-                } else {
-                    console.log('User dismissed the A2HS prompt');
-                    installAlertOpen = false;
-                    installShadow.style.display = "none";
-                }
-                deferredPrompt = null;
-            })
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+                installAlertOpen = false;
+                installShadow.style.display = "none";
+            } else {
+                console.log('User dismissed the A2HS prompt');
+                installAlertOpen = false;
+                installShadow.style.display = "none";
+            }
+            deferredPrompt = null;
         })
     })
-}
-else
-{
-    installShadow.style.display = "none";
-}
+    }
+})
 
 dismissButton.addEventListener("click", function () {
     installShadow.style.display = "none";
